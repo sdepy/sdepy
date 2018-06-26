2ebe40987071d3ba7afd1b71faa2deb3cb0bafc8
m-cip <maurizio.cipollina@gmail.com>
2018-06-27 01:40:09 +0200

    v1.0.0 (not tagged yet)

--- sdepy/__init__.py
+++ sdepy/__init__.py
@@ -100,7 +100,7 @@ from .shortcuts import *
 import numpy.testing
 test = numpy.testing.Tester().test
 
-__version__ = '1.0.0rc4'
+__version__ = '1.0.0'  # untagged
 
 
 _exclude = ('np', 'numpy', 'scipy',
