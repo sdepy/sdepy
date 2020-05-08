eb8155e9bc9da1e5c2532905f5d6c8add1e5a810
m-cip <maurizio.cipollina@gmail.com>
2020-05-08 15:55:18 +0200

    Add python 3.8 classifier to 'setup.py'

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Topic :: Software Development',
             'Topic :: Scientific/Engineering',
             'Operating System :: OS Independent',
